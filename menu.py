99997b2bdd4439241a42241f33efc2076c05e48f
Chito <francisco.jara.valdes95@gmail.com>
2025-12-09 18:40:31 -0300

    Se agrego menu.py con impresiones por pantalla

---
+++ menu.py
@@ -0,0 +1,22 @@
+def menu():
+    while True:
+        print("----------------")
+        print("       Menu")
+        print("----------------")
+        print("1.- Agregar producto")
+        print("2.- Listar producto")
+        print("3.- Buscar producto")
+        print("4.- Eliminar producto")
+        print("5.- Salir")
+
+        opcion = input("Ingrese una opcion")
+
+        if opcion == "1":
+            print("agregar producto")
+            
+            
+        elif opcion == "2":
+            print("listar productos")
+
+
+
